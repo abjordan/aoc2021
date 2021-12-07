9365c0216a4613dd42b6ac587ae17177a6a6225b
Alex Jordan <abjordan@gmail.com>
2021-12-06 21:44:59 -0500

    Probably should have done it this way the first time...

--- day06/day06.py
+++ day06/day06.py
@@ -1,34 +1,68 @@
 #!/usr/bin/env python3
 
+import functools
 import sys
 
 from alive_progress import alive_bar
+from collections import Counter
+
+
+iterations = int(sys.argv[2])
 
 with open(sys.argv[1], 'r') as infile:
     start = [int(x) for x in infile.readline().strip().split(",")]
 
 # Pretty sure there's a closed form version of this, as there's no 
-# interaction between each entry in the array.
+# interaction between each entry in the array. This will probably
+# be important before very long
 
-# For each fish, it will reproduce every six days once it hits 
-# zero for the first time. So the offset for each fish is just
-# a shift to the right on the starting point for the exercise.
+# Since the fish don't interact, we can split fish out into 
+# cohorts based on their start date, as soon as they have
+# their first baby. These cohorts will be synchronized, so
+# they should be memoizable, based on how many fish were in 
+# the initial population and the remaining days. In fact,
+# they can spit out a full new cohort!
 
-iterations = int(sys.argv[2])
+# list of pairs: (internal_clock, count)
+cohorts = []
+c = Counter(start)
+for clock_val in sorted(c.keys()):
+    cohorts.append((clock_val, c[clock_val]))
 
-population = start
-new_fish = []
-print('Initial state  : {0}'.format(population))
+# Each cohort will reproduce once every seven days and create a new cohort 
+# that reproduces at that time.
 with alive_bar(iterations) as bar:
     for i in range(1, iterations+1):
-        for idx, fish in enumerate(population):
-            if fish == 0:
-                new_fish.append(8)
-                population[idx] = 6
+        new_cohort_size = 0
+        for idx, cohort in enumerate(cohorts):
+            if cohort[0] == 0:
+                new_cohort_size += cohort[1]   # each fishy has one baby fishy
+                cohorts[idx] = (6, cohort[1])
             else:
-                population[idx] = fish - 1
-        population.extend(new_fish)
-        new_fish = []
-        bar()        
+                cohorts[idx] = (cohort[0] - 1, cohort[1])
+        if new_cohort_size != 0:
+            cohorts.append((8, new_cohort_size))
+        bar()
+    
+fish_count = functools.reduce(lambda a, b: a+b, [c[1] for c in cohorts])
+print(f"After {iterations} days, there are {fish_count} fish")
 
-print(f"After {iterations} days, there are {len(population)} fish")
+# Brute force version:
+# population = start
+# new_fish = []
+# print('Initial state  : {0}'.format(population))
+# with alive_bar(iterations+1) as bar:
+#     bar()
+#     for i in range(1, iterations+1):
+#         for idx, fish in enumerate(population):
+#             if fish == 0:
+#                 new_fish.append(8)
+#                 population[idx] = 6
+#             else:
+#                 population[idx] = fish - 1
+#         population.extend(new_fish)
+#         print(f"{population}")
+#         new_fish = []
+#         bar()
+# 
+# print(f"After {iterations} days, there are {len(population)} fish")
