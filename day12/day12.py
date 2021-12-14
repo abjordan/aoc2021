b9fb45722b017d122ff973dd4490144f6ad8247e
Alex Jordan <abjordan@gmail.com>
2021-12-13 23:02:40 -0500

    Allow a single repeat

--- day12/day12.py
+++ day12/day12.py
@@ -4,21 +4,28 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import sys
 
-def spelunk(world, current, path_to_here, visited):
+def spelunk(world, current, path_to_here, visited, doubled):
     path_to_here.append(current)
     if not current.isupper():
         visited.add(current)
+
     if current == 'end':
         return [path_to_here]
+    
     paths_from_here = []
     for next in list(world[current]):    # neighbors
         if next.isupper():
-            paths_from_here.extend(spelunk(world, next, path_to_here.copy(), visited.copy()))
+            paths_from_here.extend(spelunk(world, next, path_to_here.copy(), visited.copy(), doubled))
         elif next not in visited:
-            paths_from_here.extend(spelunk(world, next, path_to_here.copy(), visited.copy()))
+            paths_from_here.extend(spelunk(world, next, path_to_here.copy(), visited.copy(), doubled))
+        elif doubled is False and next != 'start':
+            paths_from_here.extend(spelunk(world, next, path_to_here.copy(), visited.copy(), True))
+    
     return paths_from_here
 
 if __name__ == '__main__':
+
+    allow_double = len(sys.argv) < 3
 
     world = nx.Graph()
     with open(sys.argv[1], 'r') as infile:
@@ -31,8 +38,8 @@ if __name__ == '__main__':
 
     # Find all paths that start at `start` and end at `end`
     # but only visit any particular big cave (uppercase) once
-    paths = spelunk(world, 'start', [], set(['start']))
-    for path in paths:
-        print('\t' + ','.join(path))
+    paths = spelunk(world, 'start', [], set(['start']), allow_double)
+    #for path in paths:
+    #    print('\t' + ','.join(path))
 
     print(f'Found {len(paths)} paths')
